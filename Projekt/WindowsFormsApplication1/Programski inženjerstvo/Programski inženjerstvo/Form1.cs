401af3946ffe2115854f75206171053ae75bc4dc
nkrsnik2 <nkrsnik2@foi.hr>
2014-04-26 00:11:36 +0200

    Prva verzija aplikacije dorađena
    Dorađena prva verzija aplikacije

---
+++ Projekt/WindowsFormsApplication1/Programski inženjerstvo/Programski inženjerstvo/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programski_inženjerstvo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void prijavaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PrijavaUSustav newForm = new PrijavaUSustav();
+            this.Hide();
+            newForm.Show();
+            this.Show();
+        }
+
+        private void prijavaToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            PrijavaUSustav newForm = new PrijavaUSustav();
+            this.Hide();
+            newForm.Show();
+            this.Show();
+        }
+
+        private void kreiranjeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Narudzbenica newForm = new Narudzbenica();
+            this.Hide();
+            newForm.Show();
+            this.Show();
+        }
+
+       
+    }
+}
