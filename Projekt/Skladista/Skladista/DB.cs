02aa91b4666d8cb2b8af4c91456c2a3543bbe826
nmiljanc <nmiljanc@foi.hr>
2014-05-11 21:19:48 +0200

    test_DB
    kreiranje klase DB, test

---
+++ Projekt/Skladista/Skladista/DB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skladista
+{
+    class DB
+    {
+        private static DB instance;
+        private string connectionString;
+        //private SQLiteConnection connection;
+
+    }
+}
