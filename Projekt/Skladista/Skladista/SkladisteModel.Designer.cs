b576c95130542f0d7d6962b22424701e1bb87a20
nmiljanc <nmiljanc@foi.hr>
2014-07-07 17:58:05 +0200

    Brisanje
    Brisanje

--- Projekt/Skladista/Skladista/SkladisteModel.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'C:\Users\Nikola-m\Documents\GitHub\RepositoryTeam24\Projekt\Skladista\Skladista\SkladisteModel.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
